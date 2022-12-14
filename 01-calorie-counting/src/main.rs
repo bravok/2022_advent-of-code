e7763df34a8b230bf2badd88a34d8831a734a862
Kris <kris.bravo@kachug.com>
2022-12-14 01:33:32 -0500

    Day 1 Part 2 implementation.

--- 01-calorie-counting/src/main.rs
+++ 01-calorie-counting/src/main.rs
@@ -26,11 +26,23 @@ impl Elf {
 }
 
 fn part_1(input: &str) -> u64 {
-    let split_by_elf = input.trim()git .split("\n\n");
+    let split_by_elf = input.trim().split("\n\n");
 
     let elves: Vec<Elf> = split_by_elf.map(|s| s.trim()).map(Elf::parse).collect();
 
     elves.iter().map(|e| e.calories()).max().unwrap()
+}
+
+fn part_2(input: &str) -> u64 {
+    let split_by_elf = input.trim().split("\n\n");
+
+    let elves: Vec<Elf> = split_by_elf.map(|s| s.trim()).map(Elf::parse).collect();
+    let mut calories: Vec<u64> = elves.iter().map(|e| e.calories()).collect();
+
+    calories.sort();
+    calories.reverse();
+
+    calories[0..3].iter().sum()
 }
 
 fn main() {
@@ -43,4 +55,15 @@ fn main() {
     let my_part_1_ans = part_1(my_input);
 
     println!("My Input Part 1 Answer: {my_part_1_ans}");
+    println!("\n\n");
+
+    let example_input = include_str!("example.input");
+    let example_part_2_ans = part_2(example_input);
+
+    println!("Example Input Part 2 Answer: {example_part_2_ans}");
+
+    let my_input = include_str!("my.input");
+    let my_part_2_ans = part_2(my_input);
+
+    println!("My Input Part 2 Answer: {my_part_2_ans}");
 }
